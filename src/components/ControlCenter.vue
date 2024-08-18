adb48d4ff2e54f7d80b229f3301b78194b09311a
risu <cleber.risu@outlook.com>
2024-08-18 15:32:33 -0300

    using the setup method

---
+++ src/components/ControlCenter.vue
@@ -0,0 +1,19 @@
+<script>
+import { useStore } from 'vuex';
+
+export default {
+  setup() {
+    const store = useStore();
+
+    function inc() {
+      store.dispatch('increment');
+    }
+
+    return { inc };
+  },
+};
+</script>
+
+<template>
+  <button @click="inc">Increment</button>
+</template>
---
+++ src/components/TheCounter.vue
@@ -0,0 +1,20 @@
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+  setup() {
+    const store = useStore();
+
+    const counter = computed(() => {
+      return store.getters.counter;
+    });
+
+    return { counter };
+  },
+};
+</script>
+
+<template>
+  <h2>{{ counter }}</h2>
+</template>
